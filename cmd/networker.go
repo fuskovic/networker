5377613155e47752a4234f9e2d4d70e9b7edd935
Faris Huskovic <fhuskovic92@gmail.com>
2020-01-14 08:36:39 -0500

    initial commit

---
+++ cmd/networker.go
@@ -0,0 +1,25 @@
+package cmd
+
+import (
+	"log"
+
+	"github.com/spf13/cobra"
+)
+
+// Networker is the root cmd.
+var Networker = &cobra.Command{
+	Use:  "networker",
+	Long: "networker is an easy to use networking CLI tool",
+	Run: func(cmd *cobra.Command, args []string) {
+		if err := cmd.Usage(); err != nil {
+			log.Fatalf("failed to print usage - %v\n", err)
+		}
+	},
+}
+
+// Execute runs the root cmd.
+func Execute() {
+	if err := Networker.Execute(); err != nil {
+		log.Fatalf("failed to execute networker - err : %v\n", err)
+	}
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/fuskovic/networker/cmd"
+
+func main() {
+	cmd.Networker.Execute()
+}
