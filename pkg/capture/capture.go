d0bb64676610b93987725cf632fd1c53068dcd63
Faris Huskovic <fhuskovic92@gmail.com>
2020-01-15 20:36:16 -0500

    add more err handling and forward all captured packets into a centralized channel

--- pkg/capture/capture.go
+++ pkg/capture/capture.go
@@ -1,6 +1,7 @@
 package capture
 
 import (
+	"context"
 	"fmt"
 	"time"
 
@@ -9,34 +10,53 @@ import (
 )
 
 const (
-	snapshotLen int32 = 1024
+	snapshotLen int32 = 65535
 	promiscuous       = false
 )
 
 // Packets captures network packets for the designated devices.
-func Packets(devices []string, seconds int64) error {
+func Packets(designatedDevices []string, seconds int64) error {
 	timeOut := time.Duration(seconds) * time.Second
+	ctx, cancel := context.WithTimeout(context.Background(), timeOut)
+	defer cancel()
 
 	allDevices, err := p.FindAllDevs()
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to find devices - err : %v", err)
 	}
 
 	if len(allDevices) == 0 {
 		return fmt.Errorf("no connected devices")
 	}
 
-	for _, designatedDevice := range devices {
-		for _, currentDevice := range allDevices {
-			if currentDevice.Name == designatedDevice {
-				go cap(currentDevice.Name, timeOut)
+	if len(designatedDevices) == 0 {
+		return fmt.Errorf("no designated devices")
+	}
+
+	packetChan := make(chan gopacket.Packet)
+
+	go func(ctx context.Context) {
+		for _, designatedDevice := range designatedDevices {
+			for _, currentDevice := range allDevices {
+				if currentDevice.Name == designatedDevice {
+					go cap(ctx, currentDevice.Name, timeOut, packetChan)
+				}
 			}
 		}
+	}(ctx)
+
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("capture complete")
+			return nil
+		case packet := <-packetChan:
+			fmt.Println(packet)
+		}
 	}
-	return nil
 }
 
-func cap(device string, timeOut time.Duration) error {
+func cap(ctx context.Context, device string, timeOut time.Duration, ch chan gopacket.Packet) error {
 	handle, err := p.OpenLive(device, snapshotLen, promiscuous, timeOut)
 	if err != nil {
 		return err
@@ -45,8 +65,12 @@ func cap(device string, timeOut time.Duration) error {
 
 	src := gopacket.NewPacketSource(handle, handle.LinkType())
 
-	for packet := range src.Packets() {
-		fmt.Println(packet)
+	for {
+		select {
+		case packet := <-src.Packets():
+			ch <- packet
+		case <-ctx.Done():
+			return nil
+		}
 	}
-	return nil
 }
