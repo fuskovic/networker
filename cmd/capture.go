a3ab5b3af458f6f26c00689db444029c245f0669
Faris Huskovic <fhuskovic92@gmail.com>
2020-01-16 19:50:05 -0500

    add flags for limiting pkts to capture and setting an out file name

--- cmd/capture.go
+++ cmd/capture.go
@@ -1,30 +1,64 @@
 package cmd
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/fuskovic/networker/pkg/capture"
+	"github.com/google/gopacket/pcapgo"
 	"github.com/spf13/cobra"
 )
 
 var (
-	devices []string
-	seconds int64
-	example = "networker capture eth0 -s 10"
+	devices      []string
+	seconds      int64
+	longExample  = "long form:\n./networker capture --devices en1 --seconds 10 --out myCaptureSession --limit --num 100"
+	shortExample = "short form:\n./networker c -d en1 -s 10 -o myCaptureSession -l -n 100"
+	outFile      string
+	limit        bool
+	numToCapture int64
+	writer       *pcapgo.Writer
+	file         *os.File
+	err          error
 
 	captureCmd = &cobra.Command{
 		Use:     "capture",
 		Aliases: []string{"c", "cap"},
 		Short:   "capture network packets on specified devices.",
-		Example: example,
+		Example: fmt.Sprintf("%s\n%s\n", longExample, shortExample),
 		Run: func(cmd *cobra.Command, args []string) {
-			if seconds < 5 {
-				log.Printf("capture must be at least 5 seconds long - your input : %d\n", seconds)
+			if len(devices) == 0 {
+				log.Printf("no designated devices")
+				cmd.Usage()
 				return
 			}
 
-			if err := capture.Packets(devices, seconds); err != nil {
+			if seconds < 5 {
+				log.Printf("capture must be at least 5 seconds long - your input : %d\n", seconds)
+				cmd.Usage()
+				return
+			}
+
+			if limit && numToCapture < 1 {
+				log.Printf("use of --limit flag without use of --num flag\nPlease specify number of packets to limit capture\nminimum is 1")
+				cmd.Usage()
+				return
+			}
+
+			if outFile != "" {
+				file, writer, err = capture.NewWriter(outFile)
+				if err != nil {
+					log.Printf("failed to create a new writer - err : %v\n", err)
+					cmd.Usage()
+					return
+				}
+				defer file.Close()
+			}
+
+			if err := capture.Packets(devices, seconds, numToCapture, writer, limit); err != nil {
 				log.Printf("error during packet capture : %v\n", err)
+				cmd.Usage()
 				return
 			}
 		},
@@ -33,7 +67,10 @@ var (
 
 func init() {
 	captureCmd.PersistentFlags().Int64VarP(&seconds, "seconds", "s", 0, "Amount of seconds to run capture")
+	captureCmd.PersistentFlags().StringSliceVarP(&devices, "devices", "d", []string{}, "devices on which to capture network packets (comma separated).")
+	captureCmd.PersistentFlags().StringVarP(&outFile, "out", "o", "", "specify outfile to write captured packets to")
+	captureCmd.PersistentFlags().BoolVarP(&limit, "limit", "l", false, "enable packet capture limiting(must use with --num || -n to specify number).")
+	captureCmd.PersistentFlags().Int64VarP(&numToCapture, "num", "n", 0, "number of packets to capture (accumulative for all devices)")
 	captureCmd.MarkPersistentFlagRequired("seconds")
-	captureCmd.Flags().StringSliceVarP(&devices, "devices", "d", []string{}, "devices on which to capture network packets (comma separated).")
 	Networker.AddCommand(captureCmd)
 }
--- pkg/capture/capture.go
+++ pkg/capture/capture.go
@@ -3,10 +3,13 @@ package capture
 import (
 	"context"
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
 	p "github.com/google/gopacket/pcap"
+	"github.com/google/gopacket/pcapgo"
 )
 
 const (
@@ -15,7 +18,7 @@ const (
 )
 
 // Packets captures network packets for the designated devices.
-func Packets(designatedDevices []string, seconds int64) error {
+func Packets(designatedDevices []string, seconds, num int64, writer *pcapgo.Writer, isLimited bool) error {
 	timeOut := time.Duration(seconds) * time.Second
 	ctx, cancel := context.WithTimeout(context.Background(), timeOut)
 	defer cancel()
@@ -25,33 +28,37 @@ func Packets(designatedDevices []string, seconds int64) error {
 		return fmt.Errorf("failed to find devices - err : %v", err)
 	}
 
-	if len(allDevices) == 0 {
-		return fmt.Errorf("no connected devices")
-	}
-
-	if len(designatedDevices) == 0 {
-		return fmt.Errorf("no designated devices")
-	}
-
 	packetChan := make(chan gopacket.Packet)
 
-	go func(ctx context.Context) {
-		for _, designatedDevice := range designatedDevices {
-			for _, currentDevice := range allDevices {
-				if currentDevice.Name == designatedDevice {
-					go cap(ctx, currentDevice.Name, timeOut, packetChan)
-				}
+	for _, designatedDevice := range designatedDevices {
+		for _, currentDevice := range allDevices {
+			if currentDevice.Name == designatedDevice {
+				go cap(ctx, currentDevice.Name, timeOut, packetChan)
 			}
 		}
-	}(ctx)
+	}
+
+	var pktsCaptured int64
 
 	for {
 		select {
 		case <-ctx.Done():
 			fmt.Println("capture complete")
 			return nil
-		case packet := <-packetChan:
-			fmt.Println(packet)
+		case p := <-packetChan:
+			if writer != nil {
+				if err := writer.WritePacket(p.Metadata().CaptureInfo, p.Data()); err != nil {
+					return fmt.Errorf("failed to write to pcap - err : %v", err)
+				}
+			} else {
+				fmt.Println(p)
+			}
+
+			pktsCaptured++
+			if limitReached(isLimited, num, pktsCaptured) {
+				fmt.Println("limit reached")
+				cancel()
+			}
 		}
 	}
 }
@@ -63,6 +70,8 @@ func cap(ctx context.Context, device string, timeOut time.Duration, ch chan gopa
 	}
 	defer handle.Close()
 
+	time.Sleep(time.Second)
+
 	src := gopacket.NewPacketSource(handle, handle.LinkType())
 
 	for {
@@ -73,4 +82,24 @@ func cap(ctx context.Context, device string, timeOut time.Duration, ch chan gopa
 			return nil
 		}
 	}
+}
+
+// NewWriter creates a new pcap and a writer for writing to it.
+func NewWriter(outFile string) (*os.File, *pcapgo.Writer, error) {
+	fileName := fmt.Sprintf("%s.pcap", outFile)
+	f, err := os.Create(fileName)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to create %s", fileName)
+	}
+
+	w := pcapgo.NewWriter(f)
+	if err := w.WriteFileHeader(uint32(snapshotLen), layers.LinkTypeEthernet); err != nil {
+		return nil, nil, fmt.Errorf("failed to write pcap header - err : %v", err)
+	}
+
+	return f, w, nil
+}
+
+func limitReached(isLimited bool, limit, captured int64) bool {
+	return isLimited && captured == limit
 }
