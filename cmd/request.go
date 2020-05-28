5a4e82be4ddee45bc6639ede9a422c3cf0769368
Faris Huskovic <fhuskovic92@gmail.com>
2020-05-27 20:29:00 -0400

    add time-out flag to request sub-cmd

--- cmd/request.go
+++ cmd/request.go
@@ -27,8 +27,9 @@ var (
 
 func init() {
 	reqCmd.Flags().StringVarP(&reqCfg.URL, "url", "u", reqCfg.URL, "URL to send request.")
-	reqCmd.Flags().StringVarP(&reqCfg.Method, "method", "m", reqCfg.Method, "Specify method. (supported methods include GET and POST)")
+	reqCmd.Flags().StringVarP(&reqCfg.Method, "method", "m", "GET", "Specify method. (default: GET - other supported methods include POST, PUT, PATCH, and DELETE)")
 	reqCmd.Flags().StringVarP(&reqCfg.Data, "data", "d", reqCfg.Data, "JSON string or file to use for request body.")
+	reqCmd.Flags().IntVarP(&reqCfg.TimeOut, "time-out", "t", 3, "Specify number of seconds for time-out. (default : 3)")
 	reqCmd.MarkFlagRequired("url")
 	reqCmd.MarkFlagRequired("method")
 	Networker.AddCommand(reqCmd)
