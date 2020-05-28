6558aa6648a0e274467862348a9f950917553d69
Faris Huskovic <fhuskovic92@gmail.com>
2020-05-27 20:35:09 -0400

    move request sub-cmd config logic to its own file

---
+++ pkg/request/config.go
@@ -0,0 +1,60 @@
+package request
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"path"
+	"strings"
+)
+
+const jsonExt = ".json"
+
+var supportedMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE"}
+
+// Config collects the command parameters for the request sub-command.
+type Config struct {
+	URL, Method, Data string
+	TimeOut           int
+}
+
+func (c *Config) validMethod() bool {
+	for _, m := range supportedMethods {
+		if m == c.Method {
+			return true
+		}
+	}
+	return false
+}
+
+func (c *Config) hasProtoScheme() bool {
+	has := func(s string) bool { return strings.Contains(c.URL, s) }
+	return has("http://") || has("https://")
+}
+
+func (c *Config) buildBody() (*bytes.Buffer, error) {
+	if c.Data == "" {
+		return nil, nil
+	}
+
+	var (
+		buf  *bytes.Buffer
+		data []byte
+		err  error
+	)
+
+	if path.Ext(c.Data) != jsonExt {
+		data = []byte(c.Data)
+	} else {
+		data, err = ioutil.ReadFile(c.Data)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	enc := json.NewEncoder(buf)
+	if err := enc.Encode(data); err != nil {
+		return nil, err
+	}
+	return buf, nil
+}
