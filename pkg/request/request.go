8a9384f6fde18661b5068d7c31451ddee69f548a
Faris Huskovic <fhuskovic92@gmail.com>
2020-05-27 20:34:50 -0400

    use http.Client for more flexibility when creating requests

--- pkg/request/request.go
+++ pkg/request/request.go
@@ -1,62 +1,18 @@
 package request
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"net/http"
 	"os"
-	"path"
-	"strings"
+	"time"
 )
-
-const jsonExt = ".json"
-
-// Config collects the command parameters for the request sub-command.
-type Config struct{ URL, Method, Data string }
-
-func (c *Config) validMethod() bool {
-	return c.Method == "GET" || c.Method == "POST"
-}
-
-func (c *Config) hasProtoScheme() bool {
-	has := func(s string) bool { return strings.Contains(c.URL, s) }
-	return has("http://") || has("https://")
-}
-
-func (c *Config) buildBody() (*bytes.Buffer, error) {
-	if c.Data == "" {
-		return nil, nil
-	}
-
-	var (
-		buf  *bytes.Buffer
-		data []byte
-		err  error
-	)
-
-	if path.Ext(c.Data) != jsonExt {
-		data = []byte(c.Data)
-	} else {
-		data, err = ioutil.ReadFile(c.Data)
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	enc := json.NewEncoder(buf)
-	if err := enc.Encode(data); err != nil {
-		return nil, err
-	}
-
-	return buf, nil
-}
 
 // Run executes the command logic for the request package.
 func Run(cfg *Config) error {
-	var resp *http.Response
+	seconds := time.Duration(cfg.TimeOut)
+	timeOut := time.Duration(seconds * time.Second)
+	client := http.Client{Timeout: timeOut}
 
 	body, err := cfg.buildBody()
 	if err != nil {
@@ -67,15 +23,16 @@ func Run(cfg *Config) error {
 		cfg.URL = "https://" + cfg.URL
 	}
 
-	switch cfg.Method {
-	case "GET":
-		resp, err = http.Get(cfg.URL)
-	case "POST":
-		resp, err = http.Post(cfg.URL, "application/json", body)
-	default:
-		return fmt.Errorf("%s is an unsupported request method", cfg.Method)
+	if !cfg.validMethod() {
+		return fmt.Errorf("%s is an invalid request method", cfg.Method)
 	}
 
+	req, err := http.NewRequest(cfg.Method, cfg.URL, body)
+	if err != nil {
+		return err
+	}
+
+	resp, err := client.Do(req)
 	if err != nil {
 		return err
 	}
