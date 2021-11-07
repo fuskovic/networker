0dc7de28fa846941ec9c3f5b83151100377ebdc5
fuskovic <fhuskovic92@gmail.com>
2021-11-07 11:43:32 -0600

    go mod tidy

--- go.mod
+++ go.mod
@@ -4,10 +4,9 @@ go 1.16
 
 require (
 	cdr.dev/coder-cli v1.17.0
-	cdr.dev/slog v1.4.0
 	github.com/ammario/ipisp v1.0.0
 	github.com/jackpal/gateway v1.0.7
 	github.com/spf13/pflag v1.0.5
-	github.com/stretchr/testify v1.7.0 // indirect
+	github.com/stretchr/testify v1.7.0
 	go.coder.com/cli v0.6.0
 )
--- internal/list/list_test.go
+++ internal/list/list_test.go
@@ -9,22 +9,37 @@ import (
 
 func TestListDevices(t *testing.T) {
 	t.Parallel()
-	// We only check the current device exists in the list of devices
-	// because its the only device guaranteed to be on the LAN.
-	// Asserting against a static list of devices is not reliable because those
-	// devices may not be on the network at anymore at any given time.
-	t.Run("find current device in list of devices", func(t *testing.T) {
-		ctx := context.Background()
-		currentDevice, err := getCurrentDevice(ctx)
-		require.NoError(t, err)
-		devices, err := Devices(ctx)
-		require.NoError(t, err)
-		var foundDevice bool
-		for _, d := range devices {
-			if d.LocalIP.String() == currentDevice.LocalIP.String() {
-				foundDevice = true
+	t.Run("ShouldPass", func(t *testing.T) {
+		// We only check the current device exists in the list of devices
+		// because its the only device guaranteed to be on the LAN.
+		// Asserting against a static list of devices is not reliable because those
+		// devices may not be on the network at anymore at any given time.
+		t.Run("find current device in list of devices", func(t *testing.T) {
+			t.Parallel()
+			ctx := context.Background()
+			currentDevice, err := getCurrentDevice(ctx)
+			require.NoError(t, err)
+			devices, err := Devices(ctx)
+			require.NoError(t, err)
+			var foundDevice bool
+			for _, d := range devices {
+				if d.LocalIP.String() == currentDevice.LocalIP.String() {
+					foundDevice = true
+				}
 			}
-		}
-		require.True(t, foundDevice)
+			require.True(t, foundDevice)
+		})
+	})
+	t.Run("ShouldFail", func(t *testing.T) {
+		t.Run("get device using invalid ip", func(t *testing.T) {
+			d, err := getDevice(context.Background(), "invalid")
+			require.Nil(t, d)
+			require.Error(t, err)
+		})
+		t.Run("get hosts using invalid CIDR", func(t *testing.T) {
+			hosts, err := getHosts(context.Background(), "invalid", nil)
+			require.Nil(t, hosts)
+			require.Error(t, err)
+		})
 	})
 }
