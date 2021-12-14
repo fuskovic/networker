7f7efe14fd4c704d4cfbf6dc3ddb69d814b361a6
fuskovic <fhuskovic92@gmail.com>
2021-12-13 18:42:05 -0600

    tls loadbalancer support working

--- internal/loadbalancer/target.go
+++ internal/loadbalancer/target.go
@@ -1,6 +1,7 @@
 package loadbalancer
 
 import (
+	"crypto/tls"
 	"errors"
 	"fmt"
 	"net"
@@ -35,12 +36,42 @@ func newTarget(protocol, host string) (*target, error) {
 
 	reverseProxy := httputil.NewSingleHostReverseProxy(url)
 	if protocol == "https" {
-		// TODO: make sure transport supports TLS
-		reverseProxy.Transport = &http.Transport{}
+		reverseProxy.Transport = &http.Transport{
+			DialTLS: tlsDialer,
+		}
 	}
 
 	return &target{
 		ReverseProxy: reverseProxy,
 		address:      host,
 	}, nil
+}
+
+func tlsDialer(network, addr string) (net.Conn, error) {
+	host, _, err := net.SplitHostPort(addr)
+	if err != nil {
+		return nil, err
+	}
+
+	conn, err := net.Dial(network, addr)
+	if err != nil {
+		return nil, err
+	}
+
+	tlsClient := tls.Client(conn,
+		&tls.Config{ServerName: host},
+	)
+
+	if err := tlsClient.Handshake(); err != nil {
+		conn.Close()
+		return nil, err
+	}
+
+	state := tlsClient.ConnectionState()
+
+	cert := state.PeerCertificates[0]
+	if err := cert.VerifyHostname(host); err != nil {
+		return nil, err
+	}
+	return tlsClient, nil
 }
