d3ba1c57eab342fec2497e9c0f65c06d64dc5abe
fuskovic <fhuskovic92@gmail.com>
2022-04-09 12:12:50 -0500

    add yaml output test to list command

--- cmd/list_test.go
+++ cmd/list_test.go
@@ -9,10 +9,11 @@ import (
 	"github.com/fuskovic/networker/internal/list"
 	"github.com/fuskovic/networker/internal/test"
 	"github.com/stretchr/testify/require"
+	"gopkg.in/yaml.v3"
 )
 
 func TestListCommand(t *testing.T) {
-	test.WithNetworker(t, "list devices as json output", func(t *testing.T) {
+	test.WithNetworker(t, "list devices output as json", func(t *testing.T) {
 		// start the list command
 		cmd := exec.Command("networker", "ls", "-o", "json")
 		stdout, err := cmd.StdoutPipe()
@@ -22,6 +23,21 @@ func TestListCommand(t *testing.T) {
 		// assert we can unmarshal the json output as expected
 		var devices []list.Device
 		require.NoError(t, json.NewDecoder(stdout).Decode(&devices))
+		require.NoError(t, cmd.Wait())
+
+		// assert that the devices are not empty
+		require.True(t, len(devices) > 0)
+	})
+	test.WithNetworker(t, "list devices output as yaml", func(t *testing.T) {
+		// start the list command
+		cmd := exec.Command("networker", "ls", "-o", "yaml")
+		stdout, err := cmd.StdoutPipe()
+		require.NoError(t, err)
+		require.NoError(t, cmd.Start())
+
+		// assert we can unmarshal the yaml output as expected
+		var devices []list.Device
+		require.NoError(t, yaml.NewDecoder(stdout).Decode(&devices))
 		require.NoError(t, cmd.Wait())
 
 		// assert that the devices are not empty
