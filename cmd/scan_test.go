19160b9b715d5ba70089f7b82f3d593b1ac88eb9
fuskovic <fhuskovic92@gmail.com>
2022-04-09 12:14:38 -0500

    add yaml output test to scan command

--- cmd/scan_test.go
+++ cmd/scan_test.go
@@ -8,10 +8,11 @@ import (
 	"github.com/fuskovic/networker/internal/ports"
 	"github.com/fuskovic/networker/internal/test"
 	"github.com/stretchr/testify/require"
+	"gopkg.in/yaml.v3"
 )
 
 func TestScanCommand(t *testing.T) {
-	test.WithNetworker(t, "output scanned devices as json output", func(t *testing.T) {
+	test.WithNetworker(t, "output scanned devices as json", func(t *testing.T) {
 		// start the list command
 		cmd := exec.Command("networker", "scan", "-o", "json")
 		stdout, err := cmd.StdoutPipe()
@@ -21,6 +22,21 @@ func TestScanCommand(t *testing.T) {
 		// assert we can unmarshal the json output as expected
 		var scanResults []ports.Scan
 		require.NoError(t, json.NewDecoder(stdout).Decode(&scanResults))
+		require.NoError(t, cmd.Wait())
+
+		// assert that the results are not empty
+		require.True(t, len(scanResults) > 0)
+	})
+	test.WithNetworker(t, "output scanned devices as yaml", func(t *testing.T) {
+		// start the list command
+		cmd := exec.Command("networker", "scan", "-o", "json")
+		stdout, err := cmd.StdoutPipe()
+		require.NoError(t, err)
+		require.NoError(t, cmd.Start())
+
+		// assert we can unmarshal the yaml output as expected
+		var scanResults []ports.Scan
+		require.NoError(t, yaml.NewDecoder(stdout).Decode(&scanResults))
 		require.NoError(t, cmd.Wait())
 
 		// assert that the results are not empty
