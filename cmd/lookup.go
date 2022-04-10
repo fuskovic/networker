fe526980e6d2c7e14d02e1bebb45f894476b4a40
fuskovic <fhuskovic92@gmail.com>
2022-04-10 03:02:12 -0500

    update networker lookup network command examples

--- cmd/lookup.go
+++ cmd/lookup.go
@@ -288,18 +288,22 @@ var lookupNetworkCmd = &cobra.Command{
 	Short: "Lookup the network address of a provided host.",
 	Example: `
 # Lookup network by ip or hostname:
+
 	networker lookup network 8.8.8.8
 	networker lookup network dns.google.
 
 # Lookup network by ip or hostname(short-hand):
+
 	nw lu n 8.8.8.8
 	nw lu n dns.google.
 
 # Lookup network by ip or hostname(short-hand) and output as json:
+
 	nw lu n 8.8.8.8 -o json
 	nw lu n dns.google. -o json
 
 # Lookup network by ip or hostname(short-hand) and output as json:
+
 	nw lu n 8.8.8.8 -o yaml
 	nw lu n dns.google. -o yaml
 
