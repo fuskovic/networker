6f92d02a8535ff98c3464b2d36c7110d3f85a0a5
fuskovic <fhuskovic92@gmail.com>
2022-04-16 16:30:48 -0500

    add dialer test

---
+++ internal/shell/dialer_test.go
@@ -0,0 +1,58 @@
+package shell
+
+import (
+	"os/exec"
+	"strconv"
+	"strings"
+	"syscall"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestDialer(t *testing.T) {
+	t.Run("ShouldFail", func(t *testing.T) {
+		t.Run("if addr is not serving shell", func(t *testing.T) {
+			require.Error(t, Dial("localhost:80"))
+		})
+	})
+	t.Run("ShouldPass", func(t *testing.T) {
+		// get the process id of the current shell
+		getShellPid := exec.Command("bash", "-c", "echo $$")
+		output, err := getShellPid.CombinedOutput()
+		require.NoError(t, err)
+		out := strings.TrimSpace(string(output))
+		ogPid, err := strconv.Atoi(out)
+		require.NoError(t, err)
+
+		go func() {
+			// start the server
+			require.NoError(t, Serve("bash", "4444"))
+		}()
+
+		// Connect to it
+		go func() {
+			require.NoError(t, Dial("localhost:4444"))
+		}()
+
+		// grace period to wait for connection to establish
+		time.Sleep(time.Second)
+
+		// get the process id of the new
+		getShellPid = exec.Command("bash", "-c", "echo $$")
+		output, err = getShellPid.CombinedOutput()
+		require.NoError(t, err)
+		out = strings.TrimSpace(string(output))
+		newPid, err := strconv.Atoi(out)
+		require.NoError(t, err)
+
+		// assert that the current shells process id is different to the original
+		require.NotEqual(t, ogPid, newPid)
+
+		// kill the server
+		require.NoError(t,
+			syscall.Kill(syscall.Getpid(), syscall.SIGINT),
+		)
+	})
+}
